9a1f173898efd1959221e9a92ad97923ef3dcf87
andrezottis <zottis.andre@outlook.com>
2022-05-05 18:14:01 -0300

    Added first route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,11 @@
 import express from 'express'
 
 const app = express();
+
+app.get('/users', (req, res)=> {
+    return res.send('Hello user');
+})
+
+app.listen(3333, () => {
+    console.log('HTTP Server running.');
+});
