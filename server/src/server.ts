81fd5225f5db2710ed4d66f3a59d6f2059a32559
andrezottis <zottis.andre@outlook.com>
2022-05-06 15:37:37 -0300

    Added mail send at backend.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,18 @@
 import express from 'express'
+import nodemailer from 'nodemailer'
 import { prisma } from  './prisma'
 const app = express();
 
 app.use(express.json());
 
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "af0647aace7b16",
+      pass: "0b8a3b7df9c560"
+    }
+  });
 
 app.post('/feedbacks', async (req, res)=> {
 const {type, comment, screenshot} = req.body;
@@ -16,6 +25,18 @@ const {type, comment, screenshot} = req.body;
         }
     })
 
+    await transport.sendMail({
+        from:'Equipe Feedwidget <oi@feedwidget.com>',
+        to:'Andre <andre@feedwidget.com>',
+        subject: "Novo feedback recebido",
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+            `<p>Tipo do Feedback: ${type} </p>`,
+            `<p>Comentário: ${comment} </p>`,
+            `<p>Screenshot: ${screenshot} </p>`,
+            `</div>`
+        ].join('\n')
+    });
 
     return res.status(201).json(feedbackFromDB);
 })
