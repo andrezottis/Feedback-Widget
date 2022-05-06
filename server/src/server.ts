69fdceb6c2af3f2dd87ea153b0f4041d2373848f
andrezottis <zottis.andre@outlook.com>
2022-05-06 14:03:42 -0300

    finished create feedback route with db.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,23 @@
 import express from 'express'
-
+import { prisma } from  './prisma'
 const app = express();
 
-app.get('/users', (req, res)=> {
-    return res.send('Hello user');
+app.use(express.json());
+
+
+app.post('/feedbacks', async (req, res)=> {
+const {type, comment, screenshot} = req.body;
+
+    const feedbackFromDB = await prisma.feedback.create({
+        data: {
+            type: type, //poderia usar short sintax aode seria apenas type, sem o :
+            comment: comment,
+            screenshot: screenshot,
+        }
+    })
+
+
+    return res.status(201).json(feedbackFromDB);
 })
 
 app.listen(3333, () => {
