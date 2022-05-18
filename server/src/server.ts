30dcc11c866e74d94d2555e8eed9ee4aad06b771
andrezottis <zottis.andre@outlook.com>
2022-05-18 14:57:00 -0300

    changed port declaration to support backend deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {  //change made to allow railway app usage.
   console.log("HTTP Server running.");
 });
